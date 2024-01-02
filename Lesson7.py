19e26330030c28aeb13e6ad517f65b48ce6cf5ad
DAPCK <pomogaybo.d@gmail.com>
2024-01-02 19:43:21 +0200

    print

--- Lesson7.py
+++ Lesson7.py
@@ -5,3 +5,32 @@
 # print(2 + (100 ** 0.5))
 x, y, z = 1, 5, 8
 print( x, y ,z )
+#bool
+my_true = True
+my_false = False
+
+print(type(my_true))
+print(type(my_false))
+
+#str (), float (), int (), bool ()
+
+x = 5.7
+print(x, type(x))
+x = int (x)
+print(x, type(x))
+x = str (x)
+print(x, type(x))
+x = 5
+x = bool (x)
+print(x, type(x))
+x = 0
+x = bool (x)
+print(x, type(x))
+x = ''
+print(x, type(x))
+x = bool (x)
+print(x, type(x))
+x = None
+print(x, type(x))
+x = bool (x)
+print(x, type(x))
---
+++ Lesson8.py
@@ -0,0 +1,27 @@
+# words = 'Hello World!'
+# print(words)
+# words = 'Hello "World!"'
+# print(words)
+# words = 'Hello , \\test "World!"'
+# # єкрануючий символ \
+# print(words)
+
+# verse = 'Go fuck yourself\n\
+# Go fuck yourself\n\
+# Go fuck yourself'
+# print(verse)
+# # \ - перенос строки без форматування
+# # \n - перенос з форматуванням
+
+# verse = ('Go fuck yourself\n'
+#          'Go fuck yourself \n'
+#          'Go fuck yourself')
+# print(verse)
+# # фраза у дужках () як одне логічне ціле
+verse = '''\
+Go fuck yourself
+Go fuck yourself
+Go fuck yourself\
+'''
+print(verse)
+# '''....''' - також вивід форматованих строк. \ - прибирання зайвих переносів
