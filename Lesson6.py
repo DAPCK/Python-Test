8b36217423cf06c84ee169ee46e5060840490007
DAPCK <pomogaybo.d@gmail.com>
2024-01-02 17:26:29 +0200

    print

---
+++ Lesson6.py
@@ -0,0 +1,12 @@
+print((2+3)/5)
+print((2+3)%5)
+print(7//2)
+print(5 ** 7)
+print(2 + (100 ** 0.5))
+X = 12
+Y = 3.5
+Z = "Motherfucker"
+print(X + 5)
+print(type (X))
+print(type (Y))
+print(type (Z))
