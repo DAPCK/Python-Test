cf62e5db2ed6f4ec9f5688e4968f7075383b7897
DAPCK <pomogaybo.d@gmail.com>
2024-01-03 11:55:31 +0200

    Операторы

---
+++ Lesson12.py
@@ -0,0 +1,70 @@
+print(2 > 3)
+# print(3 == 3)
+
+'''
+if выражение 1:
+    блок кода 1
+elif выражение 2:
+    блок кода 2
+else:
+    блок кода 3
+'''
+
+# x = 0 #0 - всегда False
+# if x:
+#      print('Переменная x вернула ИСТИНУ')
+# else:
+#      print('Переменная x вернула ЛОЖЬ')
+
+# if 1:
+#     print('Выражение истинно')
+# else:
+#     print('Выражение ложно')
+
+# light = 'green'
+#
+# if light == 'red':
+#     print('Stop')
+# elif light == 'yellow':
+#     print('Wait')
+# elif light == 'green':
+#     print('Go')
+# else:
+#     print('What?')
+
+# age = int(input('Сколько Вам лет? '))
+#
+# if age >= 18:
+#     print('Wellkonen motherfucker!')
+# elif age < 15:
+#     print('Fucck off stupid asshole')
+# else:
+#     print(f'You\'r {age} wait for {18- age} years ang go fuck your self')
+
+# time = 12
+# if time < 12 or time > 13:
+#     print('Open')
+# else:
+#     print('Close')
+
+# time = 8
+# day = 'st'
+#
+# if time >= 8 and day != 'su':
+#     print('Open')
+# else:
+#     print('Close')
+
+# x = 0
+# if not x:
+#     print('OK')
+# else:
+#     print('NO')
+
+# x = int(input('input some numbers'))
+# # # res = 'OK' if x else 'NO'
+# # # print(res)
+# print('OK' if x else 'NO')
+
+
+
