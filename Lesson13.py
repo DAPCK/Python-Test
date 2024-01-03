cc8e75c863f6fa0612d0b67bc968ac20edf44a5a
DAPCK <pomogaybo.d@gmail.com>
2024-01-03 12:21:41 +0200

    Операторы ghjljk;tybt

---
+++ Lesson13.py
@@ -0,0 +1,18 @@
+# i = 1
+# while i <= 10:
+#      print(i)
+#      i += 1
+
+# s = 'Hello world'
+# for l in s:
+#     if l == ' ':
+#         continue
+#     print(f'"{l}"', end=' ')
+
+x = input("Enter a world: ")
+for i in x:
+    if i == ' ':
+        break
+    print(i, end=' ')
+else:
+    print('\nNo spaces')
